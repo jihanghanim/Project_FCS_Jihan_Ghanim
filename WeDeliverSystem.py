fdb4dff4f60052798543c350336dd852105188a2
unknown <jihan.ghanim911@gmail.com>
2024-11-04 17:03:59 +0200

    modified cities functions

--- WeDeliverSystem.py
+++ WeDeliverSystem.py
@@ -1,11 +1,13 @@
 import random
+import pandas as pd
 
 #We Deliver System
 
 #main menu 
 class Mainmenu:
     def __init__(self):
-        self.menu=  int(input('Hello! Please enter:\n\n1 To go to the drivers\' menu'
+        self.menu=  int(input('Hello! Please enter:\n' 
+                                 + '\n1 To go to the drivers\' menu'
                                  + '\n2 To go to the cities\' menu'
                                  + '\n3 To exit the system\n'
                                  ))
@@ -52,19 +54,12 @@ class Driver:
     
     #generate a random unique id
     def genId(self):
-        start= 1
-        end=100
-        # increase range of random numbers when needed
-        while end<= len(self.drivers):
-            start +=100
-            end +=100
+        last_key = list(self.drivers.keys())[-1]
+        ids= set(self.drivers.keys())
+        id_num= int(last_key) +1
+        while id_num in ids:
+            id_num= +1
 
-        id_num= random.randint(start, end)
-        
-        #ensure a unique id is generated
-        while id_num in self.drivers:
-
-            id_num= random.randint(start, end)
         return id_num 
 
     #add the starting city of the driver
@@ -85,17 +80,20 @@ class Driver:
                 id_num= self.genId()
                 self.drivers[id_num]= {name: start_city}
                 print(f'{name}: {start_city} is added.')
+                return True
             #do not add driver and starting city to dictionary of drivers
             else:
                 print('The driver and its starting city is not added.' )
+                return False
         #starting city is available        
         else:
             id_num= self.genId()
             #add driver and starting city to dictionary of drivers
             self.drivers[id_num]= {name: start_city}
+
             print(f'{name}: {start_city} is added.')
 
-
+            return True
     #add new driver
     def addDriver(self):
 
@@ -107,7 +105,22 @@ class Driver:
             name= input("Enter the driver's name: ").capitalize()
             start_city= input("Enter the driver's start city: ").capitalize()
         #add driver and starting city
-        self.addCity(start_city, name)
+        added_driver=self.addCity(start_city, name)
+
+        if added_driver:
+            #save added driver to excel file
+            # Convert the new drivers dictionary into a list of dictionaries
+            new_data = []
+            for driver_id, info in self.drivers.items():
+                new_data.append({
+                    "ID": driver_id,
+                    "Name": list(info.keys())[0],  # Extract the name
+                    "Location": list(info.values())[0]  # Extract the location
+                })
+
+            # Create a new DataFrame from the new data
+            new_drivers_df = pd.DataFrame(new_data)
+            new_drivers_df.to_csv('drivers_data.csv', index=False)        
 
     #similar drivers
     def similarDrivers(self):
@@ -216,56 +229,12 @@ class LinkedList:
             n=Node(val,self.head)
             self.head=n
             self.size+=1
-    #print the neighbor cities of the targeted city
-    def printLL(self,dic_graph): 
-        found= False
-        temp=self.head 
-        while temp!=None:
-            if temp.info in dic_graph:
-                found=True
-                #print neighboring city name
-                print(dic_graph[temp.info])
-            temp=temp.next
-                
-        return found
-    #print the drivers delivering the targeted city
-    def printDrivers(self,dic_graph,drivers, key):
-        sim_drivers=[]
-        self.drivers=  drivers
-        found= False
-        temp=self.head
 
-        #No neighbor cities of the targted city
-        #only print the driver that has the targeted city as its starting city
-        if temp==None:
-            for id in self.drivers:
-                for name in self.drivers[id]:
-                    if self.drivers[id][name].capitalize()== dic_graph[key]:
-                        sim_drivers.append(name)
-                        found=True      
-
-        #The targted city has  neighbor cities of the targted city 
-        #print the driver that has the targeted city as its starting city
-        # and print the drivers of its neighbor cities        
-        while temp!=None:
-            if temp.info in dic_graph:
-                
-                for id in self.drivers:
-                    for name in self.drivers[id]:
-                        start_driver= self.drivers[id][name].capitalize()== dic_graph[key]
-
-                        if self.drivers[id][name].capitalize()== dic_graph[temp.info] or start_driver:
-                            sim_drivers.append(name)
-                            found=True              
-                
-            temp=temp.next
-        sim_drivers =list(set(sim_drivers))
-        print(', '.join(sim_drivers)) 
-        return found 
       
 
 class CitiesGraph():
     def __init__(self,V):
+        self.V= V
         self.list=[]
         for i in range(V):
             self.list.append(LinkedList())
@@ -273,43 +242,86 @@ class CitiesGraph():
     def add_edge(self,n1,n2):
         self.list[n1].addToHead(n2)    
         self.list[n2].addToHead(n1)
+
+    # Function to print a BFS of graph
+    def BFS(self, s, dic_graph):
+
+        # Mark all the vertices as not visited
+        visited = [False] * self.V
+
+        # Create a queue for BFS
+        queue = []
+
+
+        # Mark the source node as visited and enqueue it
+        queue.append(s)
+        visited[s] = True
+        neighbors=[]
+        while queue:
+            # Dequeue a vertex from the queue and print it
+            
+            s = queue.pop()
+            if s in dic_graph:
+                #print(dic_graph[s], end=" ")
+                neighbors.append(dic_graph[s])
+
+            # Get all adjacent vertices of the dequeued vertex s
+            # Manually traverse the linked list
+            current = self.list[s].head
+            while current is not None:
+                if not visited[current.info]:
+                    queue.append(current.info)
+                    visited[current.info] = True
+                current = current.next  # Move to the next node
+        return neighbors
+            
     #print neighbor cities
     def print_graph(self,dic_graph):
         user_city= input('Enter the city that you want to find its neighboring cities: ')
-        neighbor=False
+        city_found=False
         for key in dic_graph:
             if user_city.capitalize()== dic_graph[key]:
-                city_index=0
-                for i in self.list:  
-                    if city_index==key:       
-                        found=i.printLL(dic_graph)
-                        if found:
-                            neighbor=True
-                            break
-                    city_index +=1
-        if neighbor== False:
-            print('No neighboring cities')
+                city_found=True
+                neighbors=self.BFS(key, dic_graph)
+                if len(neighbors[1:])==0:
+                    print('No neighboring cities')
+                else:
+                    print(f'The neighboring cities of {dic_graph[key]}: ', ', '.join(neighbors[1:]))
+        if city_found==False:
+            print('This city is not included in delivery locations.')
+
     
     #print drivers delivering to this city
     def deliveringDrivers(self, dic_graph,drivers):
+        self.drivers= drivers
         user_city= input('Enter the city to find the drivers delivering to this city: ')
-        neighbor=False
+        sim_drivers=[]
         for key in dic_graph:
             if user_city.capitalize()== dic_graph[key]:
-                city_index=0
-                for i in self.list:  
-                    if city_index==key:       
-                        found=i.printDrivers(dic_graph,drivers, key)
-                        if found:
-                            neighbor=True
-                            break
-                    city_index +=1
-        if neighbor== False:
+                neighbors=self.BFS(key, dic_graph)
+                       
+                for i in neighbors:
+                    for id in self.drivers:
+                        for name in self.drivers[id]:
+                            if self.drivers[id][name].capitalize()== i.capitalize():
+                                sim_drivers.append(name)
+                print(', '.join(set(list(sim_drivers))))
+
+        if len(sim_drivers)== 0:
             print('No Drivers delivering to this city.')
 
 
 #we deliver system
-def weDeliver(drivers, dic_graph):
+def weDeliver(dic_graph):
+    
+    #read drivers data and write it in a dictionary
+
+    #drivers= {'1':{'Adam': 'Beirut'}, '2':{'Peter': 'Beirut'}, '3':{'Roy': 'Zahle'}, '4': {'Oliver': 'Jbeil'}}
+    drivers= {}
+    drivers_data= pd.read_csv('drivers_data.csv')
+    for index, row in drivers_data.iterrows():
+        drivers[(row['ID'])]={row['Name']: row['Location']}    
+    
     #mainmenu 
     m= Mainmenu()
     # go to drivers'menu
@@ -327,23 +339,23 @@ def weDeliver(drivers, dic_graph):
             drive.similarDrivers()
         #go back to main menu                    
         elif dm==4:
-            weDeliver(drivers, dic_graph) 
+            return weDeliver(dic_graph) 
         #error handling
         else:
             print('Please enter a valid choice!')
             return    
     #go to cities' menu
-    if m.menu==2:    
+    elif m.menu==2:    
         cm=m.CitiesMenu()
         #show all cities in descending order
         if cm==1:
             Cities(drivers).showCities()
         #search for cities with a given key
-        if cm==2:
+        elif cm==2:
             Cities(drivers).searchCities()
         
         #neighboring cities
-        if cm==3:
+        elif cm==3:
             #dic_graph= {0:'Akkar', 1: 'Jbeil', 2: 'Beirut', 3: 'Saida', 4: 'Zahle'}
             G=CitiesGraph(len(dic_graph))
             G.add_edge(1,2)
@@ -352,7 +364,7 @@ def weDeliver(drivers, dic_graph):
             G.print_graph(dic_graph)  
         
         #print drivers delivering to this city
-        if cm==4:
+        elif cm==4:
             #dic_graph= {0:'Akkar', 1: 'Jbeil', 2: 'Beirut', 3: 'Saida', 4: 'Zahle'}
             G=CitiesGraph(len(dic_graph))
             G.add_edge(1,2)
@@ -364,7 +376,7 @@ def weDeliver(drivers, dic_graph):
             print('Please enter a valid choice!')
         return 
     #exit system
-    if m.menu==3:
+    elif m.menu==3:
         m.exitSys()
     
     #error handling
@@ -376,12 +388,10 @@ def weDeliver(drivers, dic_graph):
 #run system
 if __name__== "__main__":
     
-    #predefined drivers with starting city
-    drivers= {'1':{'Adam': 'Beirut'}, '2':{'Peter': 'Beirut'}, '3':{'Roy': 'Zahle'}, '4': {'Oliver': 'Jbeil'}}
     #cities map
     dic_graph= {0:'Akkar', 1: 'Jbeil', 2: 'Beirut', 3: 'Saida', 4: 'Zahle'}
 
     #We dilver system
-    weDeliver(drivers, dic_graph)
+    weDeliver(dic_graph)
 
     
